20b8942a900e9a8c0fcca4828ae1582fed522bc1
Fiddlekins <fiddlekins@gmail.com>
2024-01-02 15:30:41 +0800

    - added akun dice command

--- src/commands/akun/dice.ts
+++ src/commands/akun/dice.ts
@@ -1,5 +1,18 @@
+import evaluate from 'akun-dice';
 import {ChatInputCommandInteraction} from "discord.js";
 
+function convertHtmlToMarkdown(input: string): string {
+  return input
+    .replaceAll(/<\/?i>/g, '*')
+    .replaceAll(/<\/?b>/g, '**')
+    .replaceAll(/<\/?br( \/)?>/g, '\n')
+}
+
 export async function executeDice(interaction: ChatInputCommandInteraction) {
-  await interaction.reply(`You've discovered a terrible secret...`);
+  const input = interaction.options.getString('input');
+  if (input) {
+    await interaction.reply(convertHtmlToMarkdown(evaluate(input)));
+  } else {
+    await interaction.reply(`You've discovered a terrible secret...`);
+  }
 }

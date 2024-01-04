075f2a4106e36bc8d8c13cb8a97e4d108542b702
Fiddlekins <fiddlekins@gmail.com>
2024-01-04 21:48:08 +0800

    - tidy up the cloudflare timeout errors we seem to consistently get when querying akun

--- src/commands/handlers/akun/api/tryParseJson.ts
+++ src/commands/handlers/akun/api/tryParseJson.ts
@@ -1,4 +1,13 @@
+function tryParseHTML(response: string) {
+  if (response.includes('<title>fiction.live | 524: A timeout occurred</title>')) {
+    throw new Error(`CloudFlare: 524: A timeout occurred`);
+  }
+}
+
 export function tryParseJson(response: string): unknown {
+  if (response.startsWith('<!DOCTYPE html>')) {
+    tryParseHTML(response);
+  }
   try {
     return JSON.parse(response) as unknown;
   } catch (err) {
